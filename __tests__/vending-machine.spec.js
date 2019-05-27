3177cb7d2d0ce6cd2657e1320ff0c17ce6792ab4
Nancy Chu <nancy.q.chu@gmail.com>
2019-05-26 20:30:20 -0700

    Change cash data into arrays and write dispenseChg method

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -4,7 +4,10 @@ const contents = require("../src/contents.json");
 describe("VendingMachine", () => {
   let vendingMachine = [];
   beforeEach(() => {
-    vendingMachine = new VendingMachine(contents.inventory, contents.cash);
+    vendingMachine = new VendingMachine(
+      contents.inventory,
+      contents.cashAvailable
+    );
   });
 
   describe("when asked for inventory", () => {
@@ -16,7 +19,7 @@ describe("VendingMachine", () => {
   describe("when customer didn't pay enough for product", () => {
     it("should return error message", () => {
       expect(() => vendingMachine.dispenseInv("soap", 3)).toThrow(
-        "Not enough money"
+        "Payment amount insufficient."
       );
     });
   });
@@ -34,6 +37,13 @@ describe("VendingMachine", () => {
       );
     });
   });
+  describe("when asked to dispense product that doesn't exist in vending machine", () => {
+    it("should return error", () => {
+      expect(() => vendingMachine.dispenseInv("flowers", 3)).toThrow(
+        "Sorry, this product does not exist"
+      );
+    });
+  });
   describe("when inventory is refilled and we check the inventory", () => {
     it("should be equal to the full inventory", () => {
       expect(vendingMachine.refillInv()).toEqual(contents.fullInventory);
@@ -46,7 +56,14 @@ describe("VendingMachine", () => {
   });
   describe("when cash is refilled and we check cash", () => {
     it("should be equal to the full cash", () => {
-      expect(vendingMachine.refillCash()).toEqual(contents.fullCash);
+      expect(vendingMachine.refillCash()).toEqual(contents.cashFull);
+    });
+  });
+  describe("when customer pays extra for product", () => {
+    it("should return change", () => {
+      expect(vendingMachine.dispenseInv("soap", 10)).toEqual(
+        "Enjoy your soap! Here is your change in 2 toonie coins, 3 quarter coins, 5 penny coins."
+      );
     });
   });
 });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -3,7 +3,7 @@ class VendingMachine {
     this.inventory = inventory;
     this.cash = cash;
     this.invFullQuantity = 15;
-    this.coinFullQuantity = 20;
+    this.coinFullQuantity = 50;
   }
 
   printInv() {
@@ -12,90 +12,60 @@ class VendingMachine {
   }
 
   dispenseInv(product, payment) {
+    if (!this.inventory[product]) {
+      throw new Error("Sorry, this product does not exist");
+    }
     if (this.inventory[product].quantity < 1) {
       throw new Error("Out of stock");
     }
     if (this.inventory[product].price > payment) {
-      throw new Error("Not enough money");
+      throw new Error("Payment amount insufficient.");
     }
     if (this.inventory[product].price === payment) {
       this.inventory[product].quantity--;
       return `Enjoy your ${product}!`;
     }
     if (this.inventory[product].price < payment) {
-      return `Enjoy your ${product}!`;
+      // return `Enjoy your ${product}!`;
       //dispense change function here
+      return this.dispenseChg(product, payment, this.inventory[product].price);
     }
   }
 
-  // // Create an object which hold the denominations and their values
-  // var denom = [
-  //     { name: 'ONE HUNDRED', val: 100.00},
-  //     { name: 'TWENTY', val: 20.00},
-  //     { name: 'TEN', val: 10.00},
-  //     { name: 'FIVE', val: 5.00},
-  //     { name: 'ONE', val: 1.00},
-  //     { name: 'QUARTER', val: 0.25},
-  //     { name: 'DIME', val: 0.10},
-  //     { name: 'NICKEL', val: 0.05},
-  //     { name: 'PENNY', val: 0.01}
-  //   ];
+  dispenseChg(product, payment, price) {
+    let diff = payment - price;
+    console.log(diff);
+    const change = [];
 
-  //   function checkCashRegister(price, cash, cid) {
-  //     var change = cash - price;
+    this.cash
+      .sort((a, b) => b.value - a.value)
+      .map(coin => {
+        if (diff >= coin.value && coin.quantity !== 0) {
+          let coinChangeNum = Math.floor(diff.toFixed(2) / coin.value);
+          if (coin.quantity < coinChangeNum) {
+            coinChangeNum = coin.quantity;
+          }
+          change.push({
+            denomination: coin.denomination,
+            quantity: coinChangeNum
+          });
+          diff -= (coin.value * coinChangeNum).toFixed(2);
+          coin.quantity -= coinChangeNum;
+        }
+      });
+    let chgString = "";
+    change.map(coin =>
+      coin.quantity > 1
+        ? (chgString += ` ${coin.quantity} ${coin.denomination} coins,`)
+        : (chgString += ` ${coin.quantity} ${coin.denomination} coin,`)
+    );
+    chgString = chgString.substring(0, chgString.length - 1) + ".";
+    console.log(diff);
+    console.log(change);
 
-  //     // Transform CID array into drawer object
-  //     var register = cid.reduce(function(acc, curr) {
-  //       acc.total += curr[1];
-  //       acc[curr[0]] = curr[1];
-  //       return acc;
-  //     }, {total: 0});
-
-  //     // Handle exact change
-  //     if (register.total === change) {
-  //       return 'Closed';
-  //     }
-
-  //     // Handle obvious insufficent funds
-  //     if (register.total < change) {
-  //       return 'Insufficient Funds';
-  //     }
-
-  //     // Loop through the denomination array
-  //     var change_arr = denom.reduce(function(acc, curr) {
-  //       var value = 0;
-  //       // While there is still money of this type in the drawer
-  //       // And while the denomination is larger than the change reminaing
-  //       while (register[curr.name] > 0 && change >= curr.val) {
-  //         change -= curr.val;
-  //         register[curr.name] -= curr.val;
-  //         value += curr.val;
-
-  //         // Round change to the nearest hundreth deals with precision errors
-  //         change = Math.round(change * 100) / 100;
-  //       }
-  //       // Add this denomination to the output only if any was used.
-  //       if (value > 0) {
-  //           acc.push([ curr.name, value ]);
-  //       }
-  //       return acc; // Return the current Change Array
-  //     }, []); // Initial value of empty array for reduce
-
-  //     // If there are no elements in change_arr or we have leftover change, return
-  //     // the string "Insufficient Funds"
-  //     if (change_arr.length < 1 || change > 0) {
-  //       return "Insufficient Funds";
-  //     }
-
-  //     // Here is your change, ma'am.
-  //     return change_arr;
-  //   }
-
-  //   // test here
-  //   checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
-
-  dispenseChg(payment, price) {
-    const diff = payment - price;
+    console.log(product);
+    console.log(this.cash);
+    return `Enjoy your ${product}! Here is your change in${chgString}`;
   }
 
   refillInv() {
